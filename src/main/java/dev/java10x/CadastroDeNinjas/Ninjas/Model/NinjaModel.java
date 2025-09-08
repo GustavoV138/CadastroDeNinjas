0b4e25441c0bed08d83e0361455004ca07c71a03
Gustavo Vieira <joao.g138+github@gmail.com>
2025-09-08 14:50:44 -0300

    Modelagem DB, Entity MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -1,6 +1,8 @@
-package dev.java10x.CadastroDeNinjas.model;
+package dev.java10x.CadastroDeNinjas.Ninjas.Model;
 
+import dev.java10x.CadastroDeNinjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -9,10 +11,15 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
     private String email;
     private int idade;
+
+    // Um ninja tem uma unica missao. Lê-se Muitos ninjas para uma missão
+    @ManyToOne
+    // Juntando as tabelas numa nova coluna missoes_id
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
 
     public NinjaModel() {
 
@@ -24,10 +31,16 @@ public class NinjaModel {
         this.idade = idade;
     }
 
+    public NinjaModel (String nome, String email, int idade, MissoesModel missoes) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.missoes = missoes;
+    }
+
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -35,7 +48,6 @@ public class NinjaModel {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -43,8 +55,14 @@ public class NinjaModel {
     public int getIdade() {
         return idade;
     }
-
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
     }
 }
